3bd4fa13b3ec636e1064ca046ba32c0ac7f96a28
Hybris95 <hybris_95@hotmail.com>
2024-10-29 10:22:28 +0100

    fix: Add support for Linux compilation

---
+++ build.bat
@@ -0,0 +1,3 @@
+@echo off
+pyinstaller --noconfirm --onedir --windowed --icon ".\resources\uextrader_icon_resized_idL_icon.ico" --add-data ".\resources;resources/"  ".\main.py"
+
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pyinstaller --noconfirm --onedir --windowed --icon ".\resources\uextrader_icon_resized_idL_icon.ico" --add-data ".\resources;resources/"  ".\main.py"
+pyinstaller --noconfirm --onedir --windowed --icon=./resources/uextrader_icon_resized_idL_icon.ico --add-data=./resources/*:resources/*  "./main.py"
