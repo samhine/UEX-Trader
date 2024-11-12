e761036fc448bf315e343072d07459cf5f95a914
Hybris95 <hybris_95@hotmail.com>
2024-11-12 14:59:14 +0100

    fix(test_config_manager.py): Rework unit tests of ConfigManager

--- test_config_manager.py
+++ test_config_manager.py
@@ -7,41 +7,56 @@ from config_manager import ConfigManager
 def config_file(tmpdir):
     config_path = tmpdir.join("config.ini")
     with open(config_path, "w") as f:
-        f.write("[API]\nkey=\nsecret_key=\n[SETTINGS]\nis_production=False\n\
-                debug=False\nappearance_mode=Light\n[GUI]\nwindow_width=800\nwindow_height=600\n")
+        f.write("[API]\nkey=storedKey\nsecret_key=storedSecretKey\n[SETTINGS]\nis_production=False\n\
+                debug=True\nappearance_mode=Light\n[GUI]\nwindow_width=1024\nwindow_height=768\n")
     return config_path
 
 
 @pytest.fixture
 def config_manager(config_file):
-    return ConfigManager(config_file)
+    if ConfigManager._instance is None:
+        return ConfigManager(config_file)
+    else:
+        return ConfigManager._instance
 
 
 def test_get_api_key(config_manager):
+    assert config_manager.get_api_key() == "storedKey"
     config_manager.set_api_key("test_key")
     assert config_manager.get_api_key() == "test_key"
 
 
 def test_get_secret_key(config_manager):
+    assert config_manager.get_secret_key() == "storedSecretKey"
     config_manager.set_secret_key("test_secret_key")
     assert config_manager.get_secret_key() == "test_secret_key"
 
 
 def test_get_is_production(config_manager):
+    assert config_manager.get_is_production() is False
     config_manager.set_is_production(True)
     assert config_manager.get_is_production() is True
+    config_manager.set_is_production(False)
+    assert config_manager.get_is_production() is False
 
 
 def test_get_debug(config_manager):
+    assert config_manager.get_debug() is True
+    config_manager.set_debug(False)
+    assert config_manager.get_debug() is False
     config_manager.set_debug(True)
     assert config_manager.get_debug() is True
 
 
 def test_get_appearance_mode(config_manager):
+    assert config_manager.get_appearance_mode() == "Light"
     config_manager.set_appearance_mode("Dark")
     assert config_manager.get_appearance_mode() == "Dark"
+    config_manager.set_appearance_mode("Light")
+    assert config_manager.get_appearance_mode() == "Light"
 
 
 def test_get_window_size(config_manager):
-    config_manager.set_window_size(1024, 768)
     assert config_manager.get_window_size() == (1024, 768)
+    config_manager.set_window_size(800, 600)
+    assert config_manager.get_window_size() == (800, 600)
